1569556355adedb3028ce2bc8473b065ce1bb953
Giorgio Zoppi <sba23101@student.cct.ie>
2024-12-27 19:50:31 +0000

    Fix main and makefile: snakecase and various fixes

---
+++ keyfort/models/keyfort.py
@@ -0,0 +1,49 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+
+
+class Metadata(BaseModel):
+    """Data model that implements the metadata for a secret."""
+
+    created_at: str
+    updated_at: str
+    is_active: bool
+    description: Optional[str] = ""
+
+
+class Version(BaseModel):
+    """Each secret changes version to any modification."""
+
+    version_number: int = 0
+    last_modified: str
+    description: Optional[str] = ""
+
+    def __add__(self, number: int):
+        """Operator overriding"""
+        if not isinstance(number, int):
+            raise ValueError("Invalid operator type")
+        self.version_number = self.version_number + number
+
+
+class Secret(BaseModel):
+    """Model that contains the secret."""
+
+    secret_id: str
+    value: str
+    version: Version
+    description: Optional[str] = ""
+    metadata: Optional[Metadata] = None
+
+
+class CreateSecretResponse(BaseModel):
+    secret: Secret
+
+
+class CreateSecretPayload(BaseModel):
+    secret: str = Field(min_length=1, max_length=3000)
+    value: str
+    metadata: Optional[Metadata] = None
+
+
+class UpdateSecretPayload(BaseModel):
+    secret: str
